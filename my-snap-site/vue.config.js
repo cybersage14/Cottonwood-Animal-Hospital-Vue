77bf533eceb45d9169b637ab65cbad402f2fa8de
Daren Shamoun <darenshamoun@gmail.com>
2023-04-21 15:49:27 -0700

    Added github pages integration

--- my-snap-site/vue.config.js
+++ my-snap-site/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/my-snap-site/'
+    : '/',
   transpileDependencies: true
 })
