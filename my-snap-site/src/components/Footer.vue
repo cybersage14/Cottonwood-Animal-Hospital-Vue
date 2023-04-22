df3637594ff8e065cdd191abbaa8282dcbeadbad
Daren Shamoun <darenshamoun@gmail.com>
2023-04-22 16:01:43 -0700

    added links to social media into footer

--- my-snap-site/src/components/Footer.vue
+++ my-snap-site/src/components/Footer.vue
@@ -4,6 +4,18 @@
         <p>Little Cottonwood Animal Hospital &copy; 2023</p>
         <!-- Add more footer content here -->
       </div>
+      <div class="social-buttons">
+        <a href="https://www.instagram.com/littlecottonwoodah" target="_blank">
+          <button>Instagram</button>
+        </a>
+        <a href="https://www.snapchat.com" target="_blank">
+          <button>Snapchat</button>
+        </a>
+        <a href="https://www.facebook.com/LittleCottonwoodAH" target="_blank">
+          <button>Facebook</button>
+        </a>
+      </div>
+
     </footer>
   </template>
   
