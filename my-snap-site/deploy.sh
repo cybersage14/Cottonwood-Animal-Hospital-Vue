5389ac759e56333b519df1fbd09a294dfc4c8035
Daren Shamoun <darenshamoun@gmail.com>
2023-04-23 15:08:24 -0700

    Deployment changes

--- my-snap-site/deploy.sh
+++ my-snap-site/deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
--- my-snap-site/vue.config.js
+++ my-snap-site/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/my-snap-site/' : '/',
+  publicPath: process.env.NODE_ENV ===  '/',
   transpileDependencies: true,
   pages: {
     index: {
