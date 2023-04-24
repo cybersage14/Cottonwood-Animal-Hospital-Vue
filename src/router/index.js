a01d10915ca0e247dd679f3c0e0e58246d892324
Daren Shamoun <darenshamoun@gmail.com>
2023-04-23 19:22:06 -0700

    changed router to hashing mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../components/Home.vue';
 import About from '../components/About.vue';
 import Services from '../components/Services.vue';
 import Contact from '../components/Contact.vue';
-
 
 const routes = [
   { path: '/', component: Home },
@@ -13,7 +12,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 });
 
